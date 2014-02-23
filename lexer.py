6dc98cb723c0ffb9f89100f5ea239de856e81b42
Luca Bruno <lucabru@src.gnome.org>
2014-02-23 18:31:38 +0100

    License GPLv3

--- lexer.py
+++ lexer.py
@@ -1,3 +1,18 @@
+# This file is part of SM (Stream Manipulator).
+#
+# SM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# SM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 import ttype
 import io
 
@@ -43,12 +58,12 @@ class Lexer:
 		if not c:
 			return Token (ttype.EOF)
 
-		if c == '#':
+		while c == '#':
 			# comment
-			while self.read() != '\n':
-				continue
-			c = self.peek()
-			self.read()
+			while self.peek() != '\n':
+				self.read()
+			self.skip_spaces ()
+			c = self.read()
 
 		if c.isalpha():
 			id = c

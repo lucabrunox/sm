e2dc3061900199e623b62ee5a914c9a45b43457d
Luca Bruno <lucabru@src.gnome.org>
2014-02-25 01:23:14 +0100

    remove useless \n when emitting ir

--- c/main.c
+++ c/main.c
@@ -7,11 +7,11 @@ int main() {
 	
 	SmCode* code = sm_code_new ();
 	sm_code_emit (code, "@.str = private constant [7 x i8] c\"hello\\0A\\00\"");
-	sm_code_emit (code, "define void @_smc_main() {\n");
-	sm_code_emit (code, "%1 = call i32 (i8*, ...)* @printf(i8* getelementptr ([7 x i8]* @.str, i32 0, i32 0))\n");
-	sm_code_emit (code, "ret void\n");
-	sm_code_emit (code, "}\n");
-	sm_code_emit (code, "declare i32 @printf(i8*, ...) #1\n");
+	sm_code_emit (code, "define void @_smc_main() {");
+	sm_code_emit (code, "%1 = call i32 (i8*, ...)* @printf(i8* getelementptr ([7 x i8]* @.str, i32 0, i32 0))");
+	sm_code_emit (code, "ret void");
+	sm_code_emit (code, "}");
+	sm_code_emit (code, "declare i32 @printf(i8*, ...)");
 
 	SmJit* mod = sm_jit_compile ("<stdin>", sm_code_get_unref (&code));
 	if (!mod) {

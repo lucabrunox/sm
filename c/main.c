622adfc1ed25c58f7e685122f73b57aef2dba7d4
Luca Bruno <luca.bruno@immobiliare.it>
2014-02-25 16:54:33 +0100

    initial macros

--- c/main.c
+++ c/main.c
@@ -3,6 +3,11 @@
 #include "code.h"
 
 #define EMIT(s) sm_code_emit(code, s)
+#define DEFINE_STRUCT(s,fields) EMIT("%struct." s " = type { " fields " }")
+#define STRUCT(s) "%struct." s
+#define FUNC(s) "@_smc_" s
+#define DEFINE_FUNC(ret,name,params) EMIT("define " ret " @_smc_" name "(" params ") {")
+#define END_FUNC() EMIT("}")
 
 int main() {
 	sm_jit_init ();
@@ -11,9 +16,9 @@ int main() {
 	EMIT ("@.str = private constant [7 x i8] c\"hello\\0A\\00\"");
 	EMIT ("declare i32 @printf(i8*, ...)");
 	EMIT ("declare i8* @malloc(i32)");
-	
-	EMIT ("%struct.thunk = type { i32, i32 }");
-	EMIT ("define %struct.thunk* @_sm_thunk_new() {");
+
+	DEFINE_STRUCT("thunk", "i32, i32");
+	DEFINE_FUNC(STRUCT("thunk*"), "thunk_new", "");
 	EMIT ("%sizeptr = getelementptr %struct.thunk* null, i64 1, i32 0");
 	EMIT ("%size = ptrtoint i32* %sizeptr to i32");
 	EMIT ("%ptr = call i8* @malloc(i32 %size)");
@@ -23,9 +28,9 @@ int main() {
 	
 	EMIT ("define void @_smc_main() {");
 	EMIT ("%1 = call i32 (i8*, ...)* @printf(i8* getelementptr ([7 x i8]* @.str, i32 0, i32 0))");
-	EMIT ("%2 = call %struct.thunk* @_sm_thunk_new()");
+	EMIT ("%2 = call %struct.thunk* " FUNC("thunk_new") "()");
 	EMIT ("ret void");
-	EMIT ("}");
+	END_FUNC();
 	
 
 	SmJit* mod = sm_jit_compile ("<stdin>", sm_code_get_unref (&code));
@@ -33,8 +38,8 @@ int main() {
 		return 0;
 	}
 
-	sm_jit_dump_asm (mod);
-	/* sm_jit_dump_ir (mod); */
+	/* sm_jit_dump_asm (mod); */
+	sm_jit_dump_ir (mod);
 	
 	void (*entrypoint)() = (void (*)()) sm_jit_get_function (mod, "_smc_main");
 	if (!entrypoint) {
